2096d5036f9e26d024e775cd0f4e2c49210a269b
florimondmanca <florimond.manca@gmail.com>
2018-07-14 13:18:16 +0100

    scripts directly in .travis.yml

--- scripts/deploy.sh
+++
@@ -1,7 +0,0 @@
-cat ~/.ssh/authorized_keys
-
-echo "Copying dist folder to Captain shared Nginx folder on remote…"
-rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/dist root@www.florimondmanca.com:/captain/nginx-shared
-
-echo "Triggering Captain deployment…"
-captainduckduck deploy -s -h https://captain.florimondmanca.com -a www -p $CAPTAIN_PASSWORD
