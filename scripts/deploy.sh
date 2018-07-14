23812a416b5a9cea1e99f7c2772fd4f6232d9166
florimondmanca <florimond.manca@gmail.com>
2018-07-14 14:08:34 +0100

    fix captain-definition, update .travis.yml

---
+++ scripts/deploy.sh
@@ -0,0 +1,5 @@
+echo "Copying dist folder to Captain shared Nginx folder on remote…"
+rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/dist root@www.florimondmanca.com:/captain/nginx-shared
+
+echo "Triggering Captain deployment…"
+captainduckduck deploy -s -h https://captain.florimondmanca.com -a www -p $CAPTAIN_PASSWORD
