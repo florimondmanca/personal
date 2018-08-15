ea07e265968383027a4076d130706ff1941d52d5
florimondmanca <florimond.manca@gmail.com>
2018-08-15 21:44:19 +0100

    improve responsiveness of editor fields

--- src/app/editor/editor/editor.component.ts
+++ src/app/editor/editor/editor.component.ts
@@ -1,6 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewChild } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { MatDialog } from '@angular/material';
+import { MatDialog, MatTextareaAutosize } from '@angular/material';
 import { ImageUploadDialogComponent } from '../image-upload-dialog/image-upload-dialog.component';
 
 
@@ -13,8 +13,15 @@ export class EditorComponent {
 
   @Input() mdContent: string;
   @Input() control: FormControl;
+  @ViewChild('autosize') autoSize: MatTextareaAutosize;
 
   constructor(private dialog: MatDialog) { }
+
+  ngOnInit() {
+    // HACK: Force auto resize after some time
+    // This way, images and other content will have (hopefully) loaded
+    setTimeout(() => this.autoSize.resizeToFitContent(true), 1000);
+  }
 
   openUploadDialog() {
     this.dialog.open(ImageUploadDialogComponent, {
