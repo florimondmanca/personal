37924c535413479b88e3e6724bbdb826e305a321
florimondmanca <florimond.manca@gmail.com>
2018-08-27 02:16:48 +0100

    uninstall unused animations polyfill

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, BrowserTransferStateModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
@@ -48,7 +47,6 @@ import { environment } from '../environments/environment';
   imports: [
     BrowserModule.withServerTransition({ appId: 'florimondmanca-blog' }),
     BrowserTransferStateModule,
-    BrowserAnimationsModule,
     FormsModule,
     RouterModule,
     ReactiveFormsModule,
