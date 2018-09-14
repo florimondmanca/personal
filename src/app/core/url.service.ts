5ff9733be5aa2b5fcc765ad3a2d6d715362b1ad7
florimondmanca <florimond.manca@gmail.com>
2018-09-14 10:03:55 +0200

    use https by default for post card url tag

--- src/app/core/url.service.ts
+++ src/app/core/url.service.ts
@@ -3,6 +3,13 @@ import { isPlatformServer } from '@angular/common';
 import { DOCUMENT } from '@angular/platform-browser';
 
 
+interface UrlOptions {
+  secure: boolean;
+}
+
+const DEFAULT_URL_OPTIONS: UrlOptions = { secure: true };
+
+
 @Injectable({
   providedIn: 'root'
 })
@@ -13,17 +20,18 @@ export class UrlService {
     @Inject(PLATFORM_ID) private platformId: Object,
   ) { }
 
-  public fromRoot(path?: string[]): string {
+  public fromRoot(path?: string[], opts: UrlOptions = DEFAULT_URL_OPTIONS): string {
     path = path || [];
-    return [this.siteUrl, ...path].join('/');
+    return [this.getSiteUrl(opts), ...path].join('/');
   }
 
   /* Return root URL of the website */
-  public get siteUrl(): string {
+  private getSiteUrl(opts: UrlOptions): string {
     if (isPlatformServer(this.platformId)) {
       // If on the server, document is not available.
       // Use the request object we injected via ngExpressEngine (see: server.js)
-      return 'http://' + this.injector.get('request').get('host');
+      const scheme = opts.secure ? 'https' : 'http';
+      return scheme + '://' + this.injector.get('request').get('host');
     } else {
       return this.document.location.origin;
     }
