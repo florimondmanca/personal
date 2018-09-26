308d42550f06b43db674d92cebad74059a495f03
florimondmanca <florimond.manca@gmail.com>
2018-09-26 18:31:46 +0200

    configure social cards for privacy policy

--- src/app/privacy-policy/privacy-policy.component.ts
+++ src/app/privacy-policy/privacy-policy.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CardService } from 'app/social';
 
 @Component({
   selector: 'app-privacy-policy',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PrivacyPolicyComponent implements OnInit {
 
-  constructor() { }
+  constructor(private cards: CardService) { }
 
   ngOnInit() {
+    this.cards.configure({
+      title: 'Privacy Policy',
+      description: 'Learn about what data we collect, how we use it, and more.',
+    });
   }
-
 }
