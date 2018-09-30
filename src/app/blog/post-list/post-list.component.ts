14605bf52298b55de92d3eaf863c50b20c045958
florimondmanca <florimond.manca@gmail.com>
2018-09-30 15:54:15 +0200

    Scroll to top when selecting post from list

--- src/app/blog/post-list/post-list.component.ts
+++ src/app/blog/post-list/post-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Post } from '../core';
+import { ScrollService } from 'app/core';
 
 @Component({
   selector: 'app-post-list',
@@ -11,5 +12,19 @@ export class PostListComponent {
   @Input() posts: Post[] = [];
   @Input() action = '';
   @Input() ifEmpty = 'No blog posts here yet. Stay tuned!';
+
+  constructor(private scroll: ScrollService) { }
+
+  scrollTop() {
+    this.scroll.toTop();
+  }
+
+  getLink(post: Post): string[] {
+    if (this.action === 'edit') {
+      return ['/', post.slug, 'edit'];
+    } else {
+      return ['/', post.slug];
+    }
+  }
 
 }
