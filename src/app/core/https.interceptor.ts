dea35b4cd767b54e280b7096c906e6350261121e
florimondmanca <florimond.manca@gmail.com>
2018-10-10 22:57:59 +0200

    enable https interceptor only in the browser

--- src/app/core/https.interceptor.ts
+++ src/app/core/https.interceptor.ts
@@ -1,4 +1,5 @@
-import { Injectable } from '@angular/core';
+import { Injectable, PLATFORM_ID, Inject } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { Observable } from 'rxjs';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
 import { environment } from 'environments/environment';
@@ -9,12 +10,14 @@ import { environment } from 'environments/environment';
 })
 export class HttpsInterceptor implements HttpInterceptor {
 
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) { }
+
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    return next.handle(this.ensureHttpsInProduction(req));
+    return next.handle(this.ensureHttpsOnBrowserInProduction(req));
   }
 
-  private ensureHttpsInProduction(req: HttpRequest<any>) {
-    if (environment.production) {
+  private ensureHttpsOnBrowserInProduction(req: HttpRequest<any>) {
+    if (environment.production && isPlatformBrowser(this.platformId)) {
       return req.clone({
         url: req.url.replace('http://', 'https://'),
       });
