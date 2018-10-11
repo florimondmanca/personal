fa2424b704aca2d496cbb6bdcc354b4331c19a91
florimondmanca <florimond.manca@gmail.com>
2018-10-11 16:28:42 +0200

    Make infinite scroll trigger relative to post-list element

--- src/app/blogging-shared/post-list/post-list.component.ts
+++ src/app/blogging-shared/post-list/post-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, HostListener, Inject } from '@angular/core';
+import { Component, Input, HostListener, Inject, ElementRef } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
 import { of } from 'rxjs';
 import { tap, catchError, filter } from 'rxjs/operators';
@@ -19,12 +19,13 @@ export class PostListComponent {
   posts: Post[] = [];
   loadingMore = false;
 
-  SCROLL_PERCENT_TRIGGER = 0.6;
+  SCROLL_PERCENT_TRIGGER = 0.8;
 
   constructor(
     private scroll: ScrollService,
     private postService: PostService,
     @Inject(DOCUMENT) private document: any,
+    private elementRef: ElementRef,
   ) { }
 
   ngOnInit() {
@@ -39,16 +40,21 @@ export class PostListComponent {
     if (!this.hasMorePosts()) {
       return;
     }
-    if (this.getScrolledPercentage() > this.SCROLL_PERCENT_TRIGGER && !this.loadingMore) {
+    if (this.getScrolledPercentage() >= this.SCROLL_PERCENT_TRIGGER && !this.loadingMore) {
       this.loadMore();
     }
   }
 
   private getScrolledPercentage(): number {
-    const scrollTop = this.document.body.scrollTop;
-    const bodyHeight = this.document.body.scrollHeight - window.innerHeight;
-    const scrollPercentage = (scrollTop / bodyHeight);
-    return scrollPercentage;
+    // NOTE: Scroll percentage is how much of the element's height is visible in
+    // the current window.
+    const rect = this.elementRef.nativeElement.getBoundingClientRect();
+    // NOTE: may be negative if element is completely below or above the current window.
+    const visibleElementHeight = window.innerHeight - rect.y;
+    const elementHeight = rect.height;
+    // NOTE: may be < 0 or > 1, but not a problem.
+    const percentage = visibleElementHeight / elementHeight;
+    return percentage;
   }
 
   scrollTop() {
@@ -71,10 +77,8 @@ export class PostListComponent {
   loadMore() {
     this.loadingMore = true;
     this.postService.list({ url: this.paginator.next }).pipe(
-      catchError(() => {
-        this.loadingMore = false;
-        return of(false);
-      }),
+      catchError(() => of(false)),
+      tap(() => this.loadingMore = false),
       filter(Boolean),
       tap((paginator) => this.posts = this.posts.concat(paginator.results)),
       tap((paginator) => this.paginator = paginator),
