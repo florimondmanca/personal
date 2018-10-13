d13b12faf4ae2577f07e427076d076b1b039a603
florimondmanca <florimond.manca@gmail.com>
2018-10-13 22:17:10 +0200

    update fontawesome to use DEV icon

--- src/app/social/social.module.ts
+++ src/app/social/social.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material';
 import { FollowButtonComponent } from './follow-button/follow-button.component';
-import { DevIconComponent } from './dev-icon/dev-icon.component';
 
 @NgModule({
   imports: [
@@ -11,11 +10,9 @@ import { DevIconComponent } from './dev-icon/dev-icon.component';
   ],
   declarations: [
     FollowButtonComponent,
-    DevIconComponent,
   ],
   exports: [
     FollowButtonComponent,
-    DevIconComponent,
   ]
 })
 export class SocialModule { }
