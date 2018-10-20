435f0835ac75d3b054c40effadcc95a890ff7937
florimondmanca <florimond.manca@gmail.com>
2018-10-20 14:43:12 +0200

    do not use https on localhost, swap h4 for h3

--- src/app/core/https.interceptor.ts
+++ src/app/core/https.interceptor.ts
@@ -16,8 +16,12 @@ export class HttpsInterceptor implements HttpInterceptor {
     return next.handle(this.ensureHttpsOnBrowserInProduction(req));
   }
 
+  private local(req: HttpRequest<any>): boolean {
+    return req.url.includes('localhost');
+  }
+
   private ensureHttpsOnBrowserInProduction(req: HttpRequest<any>) {
-    if (environment.production && isPlatformBrowser(this.platformId)) {
+    if (environment.production && isPlatformBrowser(this.platformId) && !this.local(req)) {
       return req.clone({
         url: req.url.replace('http://', 'https://'),
       });
