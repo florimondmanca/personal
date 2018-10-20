7350994c5b98ceddb5431df09fcf8903b9d5bae9
florimondmanca <florimond.manca@gmail.com>
2018-10-21 00:46:32 +0200

    Fix Blog routes eager loading, fix sidebav flicking on mobile

--- src/app/core/sidenav.service.ts
+++ src/app/core/sidenav.service.ts
@@ -1,4 +1,5 @@
-import { Injectable, ChangeDetectorRef } from '@angular/core';
+import { Injectable, ChangeDetectorRef, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformServer } from '@angular/common';
 import { MediaMatcher } from '@angular/cdk/layout';
 import { MatSidenav } from '@angular/material';
 
@@ -10,14 +11,19 @@ export class SidenavService {
   private _sidenav: MatSidenav;
   private mobileQuery: MediaQueryList;
   private mobileQueryListener: () => void;
+  private ready = false;
 
-  constructor(private media: MediaMatcher) { }
+  constructor(
+    private media: MediaMatcher,
+    @Inject(PLATFORM_ID) private platformId: Object,
+  ) { }
 
   init(sidenav: MatSidenav, changeDetectorRef: ChangeDetectorRef) {
     this._sidenav = sidenav;
     this.mobileQuery = this.media.matchMedia('(max-width: 600px)');
     this.mobileQueryListener = () => changeDetectorRef.detectChanges();
     this.mobileQuery.addListener(this.mobileQueryListener);
+    this.ready = true;
   }
 
   destroy() {
@@ -25,7 +31,12 @@ export class SidenavService {
   }
 
   onMobile(): boolean {
-    return this.mobileQuery.matches;
+    // Always consider to be on mobile on the server, to avoid flickering during transition
+    if (isPlatformServer(this.platformId)) {
+      return true;
+    }
+    // If sidenav not ready yet, consider to be on mobile by default
+    return this.ready ? this.mobileQuery.matches : true;
   }
 
   openOnDesktop() {
