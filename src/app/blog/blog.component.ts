123ba369e943eb920c50bd84808efa4a6a00bfe8
florimondmanca <florimond.manca@gmail.com>
2018-10-21 10:08:44 +0200

    add OnPush to BlogComponent

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-blog',
   templateUrl: './blog.component.html',
   styleUrls: ['./blog.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class BlogComponent { }
