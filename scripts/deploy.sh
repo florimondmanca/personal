46a36429ba24aa6e0d6e10382f9b9078241d0002
florimondmanca <florimond.manca@gmail.com>
2019-03-17 01:14:58 +0100

    fix: add branch

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,4 +2,4 @@ echo "Copying dist folder to Captain shared Nginx folder on remote…"
 rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/dist root@captain.florimondmanca.com:/captain/data/nginx-shared
 
 echo "Deploying using CapRover CLI…"
-caprover deploy -h https://captain.florimondmanca.com -a blog -p $CAPTAIN_PASSWORD
+caprover deploy -h https://captain.florimondmanca.com -p $CAPTAIN_PASSWORD -b master -a blog
