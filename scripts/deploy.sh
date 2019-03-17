f48a838019ce1198b5f8d43fce68340d6f47f0bb
florimondmanca <florimond.manca@gmail.com>
2019-03-17 01:06:20 +0100

    fix: remove dist folder target

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 echo "Copying dist folder to Captain shared Nginx folder on remote…"
-rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/dist root@captain.florimondmanca.com:/captain/nginx-shared
+rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/dist root@captain.florimondmanca.com:/captain/data/nginx-shared
 
 echo "Deploying using CapRover CLI…"
 caprover deploy -h https://captain.florimondmanca.com -a blog -p $CAPTAIN_PASSWORD
