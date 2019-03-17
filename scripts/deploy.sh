4283c334541f25fe3f03e4b18f899f254cf07e37
florimondmanca <florimond.manca@gmail.com>
2019-03-17 01:02:04 +0100

    fix: remove -s option

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,4 +2,4 @@ echo "Copying dist folder to Captain shared Nginx folder on remote…"
 rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/dist root@captain.florimondmanca.com:/captain/nginx-shared
 
 echo "Deploying using CapRover CLI…"
-caprover deploy -s -h https://captain.florimondmanca.com -a blog -p $CAPTAIN_PASSWORD
+caprover deploy -h https://captain.florimondmanca.com -a blog -p $CAPTAIN_PASSWORD
