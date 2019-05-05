ce7f686df3fc4cb83af65baef4e73ab944478490
florimondmanca <florimond.manca@gmail.com>
2019-05-05 20:18:20 +0200

    add Procfile

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,5 @@
+git fetch --unshallow
+
 echo "--> Copy dist folder to data directory on remote"
 rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/dist root@florimond.dev:/var/lib/dokku/data/storage/blog-build
 
